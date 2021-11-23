a86965d9e194b3d6036362cf194c09f8612c7c78
“rmuftievgit config --global user.email “rasim.muftiev@live.com <rasim.muftiev@gmail.com>
2021-11-23 17:05:36 +0100

    msg with double dashes3

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("--Welcome to my  world of Java from Jenkins");
+        System.out.println("Welcome to the changed  world of Java from Jenkins");
     }
 }
